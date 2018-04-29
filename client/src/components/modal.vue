7f588e4416c5c227ba590ab15725c78e802b8a71
hayani said <hayanisaid1995@gmail.com>
2018-04-29 07:03:21 +0100

    add some

--- client/src/components/modal.vue
+++ client/src/components/modal.vue
@@ -1,9 +1,9 @@
 <template>
-	<div class="myModal" v-if='modalState'>
+	<!-- <div class="myModal" v-if='modalState'>
 	<span class="close" v-on:click='ActivateModal()'>X</span>
 	<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/265560100" style="position:absolute;top:2%;left:10%;width:80%;height:80%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 	<p><a href="https://vimeo.com/265560100">Help Us Make Etsy Better</a> from <a href="https://vimeo.com/etsyglobal">Etsy</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
-	</div>
+	</div> -->
 </template>
 <style src='../assets/css/modal.scss' lang='scss'></style>
 
