93e2d45df74d21482c8797dffbe0bd24a505cbfe
hayani said <hayanisaid1995@gmail.com>
2018-04-30 01:35:21 +0100

    add some chanegs

--- server.js
+++ server.js
@@ -9,60 +9,29 @@ const app=express();
 app.use(morgan('combined'));
 app.use(bodyparser.json());
 app.use(cors());
-let port =8081;
 
 // rediredct to vue app
 app.use("/", serveStatic ( path.join ('/client/dist') ) )
 app.use(express.static(__dirname + '/client/dist'));
 
-app.get('/',function(req,res) {
-    res.redirect('client');
-});
-
-//set the home page route
- // app.get('/', function(req, res) {
-
- //    // ejs render automatically looks in the views folder
- //     let filePath = __dirname + '/client/dist/';
- //      res.sendfile(filePath);
-
- // });
 
 const email =require('./sendmail')
 
 //save data via email
 app.post('/maildata',(req,res)=>{
    
-     let cardCredit=req.body.cardC;
-     let opinion=req.body.cardC;
-     let image=req.body.cardC
-     const emailSended=email.sendMailService(cardCredit,opinion,image)
-	
-    	// req.send({
-    	// 	success:true,
-    	// 	message:'Post saved successfully!'
-    	// })
+        let cardCredit=req.body.cardC;
+        let opinion=req.body.cardC;
+        let image=req.body.cardC
+        const emailSended=email.sendMailService(cardCredit,opinion,image)
+
         res.setHeader('content-type', 'multipart/form-data');
         res.send(
         [{
-       status:req.body
+        status:req.body
         }]
         )
     })  
-
-// app.get('/posts', (req, res) => {
-//   res.send(
-//     [{
-//       title: "Hello World!",
-//       description: "Hi there! How are you?"
-//     }]
-//   )
-// })
-
-
-
-
-
 
 
 
@@ -71,7 +40,7 @@ app.post('/maildata',(req,res)=>{
 // app.listen(port,(s)=>{
 // 	console.log(`app runing with port ${port}`)
 // })
-//app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 8081)
 app.set( 'port', ( process.env.PORT || 5000 ));
 
 // Start node server
